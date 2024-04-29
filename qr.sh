adbbff40cea55056902a351604e73c1d73596eb5
Artemiy Misyurov <14artemiy88@gmail.com>
2024-04-30 00:02:15 +0500

    fix pytohn namespase

--- qr.sh
+++ qr.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 declare -r SCREENSHOT_PATH="/run/user/1000/qr.png"
+source $HOME/.venvs/MyEnv/bin/activate
 
 slop=$(slop -f "%g") || exit 1
 read -r GEOMETRY <<< $slop
 import -window root -crop $GEOMETRY $SCREENSHOT_PATH
-python3 "$HOME"/Apps/slop_projects/qr.py | xclip -selection clipboard
+text=$(python3 "$HOME"/Apps/slop_projects/qr.py)
+kdialog --passivepopup "$text" 2
+echo $text | xclip -selection clipboard
